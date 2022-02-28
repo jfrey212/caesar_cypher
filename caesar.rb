3a8092f9ae7154f92b59aa52d040c83b16b792cb
jfrey212 <eojyerf@gmail.com>
2022-02-27 22:47:57 -0600

    Initial commit - created ruby file, added explanatory comments, defined cypher method, set input variables from user input

---
+++ caesar.rb
@@ -0,0 +1,12 @@
+#Caesar Cypher
+#Arguments - A string, integer defining how many letters to shift each character
+#Output - modified string
+
+print "Enter the phrase to encode"
+input_string = gets.chomp
+print "Enter the shift factor"
+shift_factor = gets.chomp
+
+def caesar(input_string, shift_factor)
+
+end
