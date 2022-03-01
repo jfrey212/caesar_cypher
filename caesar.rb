04c39883615abd33e02f4dab71774ef3a25b234e
jfrey212 <eojyerf@gmail.com>
2022-03-01 00:21:57 -0600

    Started pseudocoding the solution

--- caesar.rb
+++ caesar.rb
@@ -7,6 +7,12 @@ input_string = gets.chomp
 print "Enter the shift factor"
 shift_factor = gets.chomp
 
+# The function will convert the string into an array with split. Use map to 
+# convert each character into an ascii code using ord. Then increment the ord
+# code using the shift_factor. The tricky part is to wrap back to the beginning
+# of the alphabet when it reaches z. Use two ranges, one for capitals and one
+# for lower case - this allows for maintaining capitalization status and then
+# to use conditionals to stop non letter characters from being incremented.
 def caesar(input_string, shift_factor)
 
 end
